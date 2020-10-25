a8ef85bcae2f783a9984754601e4dff2c170ad3c
Maxime Goffart <maximegoffart@icloud.com>
2020-10-25 10:49:30 +0100

    Modifs report

--- plots/3a_nodes.R
+++ plots/3a_nodes.R
@@ -4,6 +4,6 @@ smarty_node <- c(log(30), log(422), log(7553))
 
 colors <- c("blue", "green", "red", "blue", "green", "red", "blue", "green", "red")
 
-barplot(cbind(dumby_node, greedy_node, smarty_node), beside = TRUE, names.arg = c("dumby", "greedy", "smarty"), col = colors, ylim = c(0,10), main = "Comparison of the expanded nodes of the different pairs", ylab = "Expanded nodes (logarithmic value)")
+barplot(cbind(dumby_node, greedy_node, smarty_node), beside = TRUE, names.arg = c("dumby", "greedy", "smarty"), col = colors, ylim = c(0,10), main = "Comparison of number of the expanded nodes of the different pairs", ylab = "Expanded nodes (logarithmic value)")
 legend("topleft", legend = c("hminimax0.py", "hminimax1.py", "hminimax2.py"), col=c("blue", "green", "red"), pch = c(15,15))
 
--- plots/3a_times.R
+++ plots/3a_times.R
@@ -4,6 +4,6 @@ smarty_time<- c(0.2405099868774414, 0.48928165435791016, 2.790019989013672)
 
 colors <- c("blue", "green", "red", "blue", "green", "red", "blue", "green", "red")
 
-barplot(cbind(dumby_time, greedy_time, smarty_time), beside = TRUE, names.arg = c("dumby", "greedy", "smarty"), col = colors, ylim = c(0,3), main = "Comparison of the computation times of the different pairs", ylab = "Expanded nodes (logarithmic value)")
+barplot(cbind(dumby_time, greedy_time, smarty_time), beside = TRUE, names.arg = c("dumby", "greedy", "smarty"), col = colors, ylim = c(0,3), main = "Comparison of the computation times of the different pairs", ylab = "Computation time")
 legend("topleft", legend = c("hminimax0.py", "hminimax1.py", "hminimax2.py"), col=c("blue", "green", "red"), pch = c(15,15))
 
