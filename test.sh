c42b6b475b096d4d74474176be738b66c9527a1e
Maxime Goffart <maximegoffart@icloud.com>
2020-10-31 11:36:50 +0100

    Script

--- test.sh
+++ test.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+agent=hminimax0.py
+
 echo -e "small/dumby"
-python run.py --layout small_adv --agentfile all_2.py --ghostagent dumby
+python run.py --layout small_adv --agentfile $agent --ghostagent dumby
 echo -e "\nsmall/greedy"
-python run.py --layout small_adv --agentfile all_2.py --ghostagent greedy
+python run.py --layout small_adv --agentfile $agent --ghostagent greedy
 echo -e "\nsmall/smarty"
-python run.py --layout small_adv --agentfile all_2.py --ghostagent smarty
+python run.py --layout small_adv --agentfile $agent --ghostagent smarty
 echo -e "\nmedium/dumby"
-python run.py --layout medium_adv --agentfile all_2.py --ghostagent dumby
+python run.py --layout medium_adv --agentfile $agent --ghostagent dumby
 echo -e "\nmedium/greedy"
-python run.py --layout medium_adv --agentfile all_2.py --ghostagent greedy
+python run.py --layout medium_adv --agentfile $agent --ghostagent greedy
 echo -e "\nmedium/smarty"
-python run.py --layout medium_adv --agentfile all_2.py --ghostagent smarty
+python run.py --layout medium_adv --agentfile $agent --ghostagent smarty
 echo -e "\nlarge/dumby"
-python run.py --layout large_adv --agentfile all_2.py --ghostagent dumby
+python run.py --layout large_adv --agentfile $agent --ghostagent dumby
 echo -e "\nlarge/greedy"
-python run.py --layout large_adv --agentfile all_2.py --ghostagent greedy
+python run.py --layout large_adv --agentfile $agent --ghostagent greedy
 echo -e "\nlarge/smarty"
-python run.py --layout large_adv --agentfile all_2.py --ghostagent smarty
+python run.py --layout large_adv --agentfile $agent --ghostagent smarty
